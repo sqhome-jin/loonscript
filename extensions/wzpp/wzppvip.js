081bdbdcff4af00b6974a8d32f59c32dcdd3d887
Jin Sun <sunjin914@gmail.com>
2026-01-17 20:50:53 +0800

    add wzpp scripts

---
+++ extensions/wzpp/wzppvip.js
@@ -0,0 +1,27 @@
+/*************************************
+
+项目名称：问真排盘
+脚本作者：chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/bzpp2\.iwzbz\.com\/api\/.+\/(user\/getvipinfo|User\/getWXPW) url script-response-body 
+
+[mitm]
+hostname = bzpp2.iwzbz.com
+
+*************************************/
+
+
+var response = JSON.parse($response.body);
+
+response.isSuccess = true;
+response.validateKey = "sqhome";
+response.data = {
+    "expires" : "2099-09-09 09:09:09",
+    "vipLevel" : 3
+  };
+
+$done({body : JSON.stringify(response)});
